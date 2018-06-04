a7cb674f13cd36ddf5bf32d9a909fdb131e406b2
SomeBlu <webmaster@someblu.com>
2018-06-04 09:02:03 -0700

    Add cert renew script

--- ssl.sh
+++ ssl.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-sudo certbot certonly --manual -d someblu.com -d *.someblu.com --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+DIR="cert"
+DOM="someblu.com"
+
+if [ -d "$DIR" ]; then
+	sudo certbot renew --server https://acme-v02.api.letsencrypt.org/directory
+else
+    sudo certbot certonly --manual -d "$DOM" -d "*.$DOM" --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+fi
+
+sudo cp -r "/etc/letsencrypt/live/$DOM" "$DIR"
