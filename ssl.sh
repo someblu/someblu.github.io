790482e986ff8e78bb8fceba4c6f55ea0160ac82
SomeBlu <webmaster@someblu.com>
2018-06-04 09:14:11 -0700

    fix cert renew script and upload cert files

--- ssl.sh
+++ ssl.sh
@@ -8,4 +8,4 @@ else
     sudo certbot certonly --manual -d "$DOM" -d "*.$DOM" --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
 fi
 
-sudo cp -r "/etc/letsencrypt/live/$DOM" "$DIR"
+sudo cp -r "/etc/letsencrypt/live/$DOM/." "$DIR"
